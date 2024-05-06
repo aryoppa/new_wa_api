e8c7c1783ead6e51abd294b8a6ba85d77e1387f6
aryoppa <aryop@upi.edu>
2024-05-06 09:35:37 +0700

    filtering incoming message

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    // apiUrl: 'http://10.239.9.85:8000/chatbot/',
-    apiUrl: 'http://0.0.0.0:8000/chatbot/',
+    apiUrl: 'http://10.239.9.85:8000/chatbot/',
+    // apiUrl: 'http://0.0.0.0:8000/chatbot/',
     port: process.env.PORT || 8001
 };
--- controller/ConnectionController.js
+++ controller/ConnectionController.js
@@ -21,121 +21,123 @@ async function connectToWhatsApp() {
         printQRInTerminal: true,
         auth: state,
         logger: pino({ level: "silent" }),
-		shouldIgnoreJid: jid => isJidBroadcast(jid)
+        shouldIgnoreJid: isJidBroadcast
     });
     store.bind(sock.ev);
     sock.multi = true;
-    sock.ev.on('connection.update', async (update) => {
-        // Connection update logic...
-        const { connection, lastDisconnect } = update;
-		if(connection === 'close') {
-            let reason = lastDisconnect && lastDisconnect.error ? new Boom(lastDisconnect.error).output.statusCode : -1;
-			switch (reason) {
-				case DisconnectReason.badSession:
-					console.log(`Bad Session File, Please Delete ${session} and Scan Again`);
-					sock.logout();
-					break;
-				case DisconnectReason.connectionClosed:
-					console.log("Connection closed, reconnecting....");
-					connectToWhatsApp();
-					break;
-				case DisconnectReason.connectionLost:
-					console.log("Connection Lost from Server, reconnecting...");
-					connectToWhatsApp();
-					break;
-				case DisconnectReason.loggedOut:
-					console.log(`Device Logged Out, deleting session and scanning again.`);
-					fs.rm('baileys_auth_info', { recursive: true });
-					connectToWhatsApp();
-					sock.logout();
-					break;
-				case DisconnectReason.restartRequired:
-					console.log("Restart Required, Restarting...");
-					connectToWhatsApp();
-					break;
-				case DisconnectReason.timedOut:
-					console.log("Connection TimedOut, Reconnecting...");
-					connectToWhatsApp();
-					break;
-				default:
-					sock.end(`Unknown DisconnectReason: ${reason}|${lastDisconnect.error}`);
-					break;
-			}
-        }else if(connection === 'open') {
-			console.log('Bot is ready!');
-			let getGroups = await sock.groupFetchAllParticipating();
-			let groups = Object.entries(getGroups).slice(0).map(entry => entry[1]);
-			return groups;
-        }
-    });
+    sock.ev.on('connection.update', handleConnectionUpdate);
     sock.ev.on("creds.update", saveCreds);
-    // 
-    sock.ev.on("messages.upsert", async ({ messages }) => {
-        const message = messages[0];
-        // console.log(message);
-        const { key, message: msgContent } = message;
-        const { remoteJid: noWa, fromMe } = key;
-        // console.log(key);
-        const check = msgContent?.extendedTextMessage?.contextInfo;
-        const targetJid = "6285880379892@s.whatsapp.net";
-        // const targetJid = "6285880379892@s.whatsapp.net";
-        const text = msgContent?.conversation || msgContent?.extendedTextMessage?.text;
-        // console.log(msgContent);
-        // if(msgContent.conversation)
-        
-        textMessage = text.replaceAll(/\p{Emoji}/gu, ''); // '--'
+    sock.ev.on("messages.upsert", handleMessagesUpsert);
+}
 
-        // console.log(textMessage);
-        // Mark the message as read
-        await sock.readMessages([key]);
+async function handleConnectionUpdate(update) {
+    const { connection, lastDisconnect } = update;
+    if (connection === 'close') {
+        const reason = lastDisconnect?.error ? new Boom(lastDisconnect.error).output.statusCode : DisconnectReason.unknown;
+        await handleDisconnect(reason);
+    } else if (connection === 'open') {
+        console.log('Bot is ready!');
+        return await fetchGroups();
+    }
+}
 
-        // Function to handle sending replies
-        const sendReply = async (content) => {
-            try {
-                const response = await makeApiRequest(content);
-                await sock.sendMessage(noWa, { text: response.data }, { quoted: message });
-            } catch (error) {
-                console.error("Error sending message:", error);
-            }
-        };
+async function handleDisconnect(reason) {
+    switch (reason) {
+        case DisconnectReason.badSession:
+            console.log(`Bad Session File, Please Delete ${session} and Scan Again`);
+            await sock.logout();
+            break;
+        case DisconnectReason.connectionClosed:
+        case DisconnectReason.connectionLost:
+        case DisconnectReason.restartRequired:
+        case DisconnectReason.timedOut:
+            console.log("Connection issue, reconnecting...");
+            connectToWhatsApp();
+            break;
+        case DisconnectReason.loggedOut:
+            console.log(`Device Logged Out, deleting session and scanning again.`);
+            await fs.rm(session, { recursive: true });
+            connectToWhatsApp();
+            break;
+        default:
+            sock.end(`Unknown DisconnectReason: ${reason}`);
+            break;
+    }
+}
 
-        // Check if the message is mentioning the target Jid or is from the target participant
-        if (check?.mentionedJid?.includes(targetJid) || check?.participant?.includes(targetJid)) {
-            if (textMessage) {
-                await sendReply(textMessage);
-            } else {
-                const nonTextMessageReply = "Mohon maaf, untuk saat ini kami hanya dapat menerima pesan text. Silahkan kirim ulang pertanyaan Anda dalam bentuk text.";
-                await sock.sendMessage(noWa, { text: nonTextMessageReply });
-            }
-        } else if (!fromMe) {
-            if (textMessage) {
-                await sendReply(textMessage);
-            } else {
-                const messageType = Object?.keys(msgContent)[0];
-                if (messageType === 'documentMessage') {
-                    try {
-                        const buffer = await downloadMediaMessage(
-                            message,
-                            'buffer',
-                            {},
-                            { reuploadRequest: sock.updateMediaMessage }
-                        );
-                        const name = msgContent?.documentMessage?.fileName;
-                        if (name) {
-                            await writeFile(`./downloads/${name}`, buffer);
-                        } else {
-                            console.log('No file name provided, cannot save the file.');
-                        }
-                    } catch (error) {
-                        console.error("Error handling document message:", error);
-                    }
-                }
-            }
+async function fetchGroups() {
+    const getGroups = await sock.groupFetchAllParticipating();
+    return Object.values(getGroups);
+}
+
+async function handleMessagesUpsert({ messages }) {
+    const message = messages[0];
+    const { key, message: msgContent } = message;
+    const { remoteJid: noWa, fromMe } = key;
+    const check = msgContent?.extendedTextMessage?.contextInfo;
+    const targetJid = "6285880379892@s.whatsapp.net";
+    const text = msgContent?.conversation || msgContent?.extendedTextMessage?.text;
+    const textMessage = text?.replaceAll(/\p{Emoji}/gu, '');
+
+    await sock.readMessages([key]);
+    console.log(message);
+    const isGroupMessage = key.remoteJid.includes("@g.us");
+    const isPersonalMessage = !fromMe;
+    const isTargetMentioned = check?.mentionedJid?.includes(targetJid) || check?.participant?.includes(targetJid);
+
+    if (isGroupMessage && isTargetMentioned) {
+        await sendReply(noWa, textMessage, message);
+    } else if (isPersonalMessage) {
+        await handleNonTargetMessage(noWa, msgContent, message, textMessage);
+    }
+}
+
+async function sendReply(noWa, textMessage, message) {
+    if (textMessage) {
+        try {
+            const response = await makeApiRequest(textMessage);
+            await sock.sendMessage(noWa, { text: response.data }, { quoted: message });
+            
+        } catch (error) {
+            console.error("Error sending message:", error);
         }
-    });
+    } else {
+        const nonTextMessageReply = "Mohon maaf, untuk saat ini kami hanya dapat menerima pesan text. Silahkan kirim ulang pertanyaan Anda dalam bentuk text.";
+        await sock.sendMessage(noWa, { text: nonTextMessageReply });
+    }
+}
 
+async function handleNonTargetMessage(noWa, msgContent, message, textMessage) {
+    
+    if (textMessage) {
+        await sendReply(noWa, textMessage, message);
+    } else {
+        const messageType = Object.keys(msgContent)[0];
+        if (messageType === 'documentMessage') {
+            await handleDocumentMessage(message, msgContent);
+        }
+    }
+}
+
+async function handleDocumentMessage(message, msgContent) {
+    try {
+        const buffer = await downloadMediaMessage(
+            message,
+            'buffer',
+            {},
+            { reuploadRequest: sock.updateMediaMessage }
+        );
+        const name = msgContent?.documentMessage?.fileName;
+        if (name) {
+            await fs.writeFile(`./downloads/${name}`, buffer);
+        } else {
+            console.log('No file name provided, cannot save the file.');
+        }
+    } catch (error) {
+        console.error("Error handling document message:", error);
+    }
 }
 
 module.exports = {
-    connectToWhatsApp,
+    connectToWhatsApp
 }
